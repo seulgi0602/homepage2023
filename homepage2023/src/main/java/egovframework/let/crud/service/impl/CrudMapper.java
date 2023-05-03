c12c0f136de2b9c076dd6a5bf22c5af6c8b0275f
Student <Student@Student-PC>
2023-05-03 16:17:23 +0900

    2023.05.03수업내용

---
+++ homepage2023/src/main/java/egovframework/let/crud/service/impl/CrudMapper.java
@@ -0,0 +1,31 @@
+package egovframework.let.crud.service.impl;
+
+import java.util.List;
+
+import egovframework.let.crud.service.CrudVO;
+import egovframework.rte.psl.dataaccess.mapper.Mapper;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+
+@Mapper("crudMapper")
+public interface CrudMapper {
+	
+	//CRUD가져오기
+	CrudVO selectCrud(CrudVO vo) throws Exception;
+		
+	//CRUD 등록
+	void insertCrud(CrudVO vo) throws Exception;
+	 
+	//CRUD 목록 가져오기
+	 List<EgovMap> selectCrudList(CrudVO vo) throws Exception;
+	 
+	//CRUD목록 수
+	 int selectCrudListCnt(CrudVO vo) throws Exception;
+	 
+	//CRUD 수정하기
+	 void updateCrud(CrudVO vo) throws Exception;
+	 
+	//CRUD데이터 삭제하기
+	 void deleteCrud(CrudVO vo) throws Exception;
+	 
+	 
+}
